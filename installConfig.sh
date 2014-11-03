4ed76670080f95a1fdb1afad66e18712472cd735
Kevin Haire <khaire@shopcity.com>
2014-11-03 15:02:16 -0500

    initial commit

---
+++ installConfig.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+brew install caskroom/cask/brew-cask
+brew install node
+
+brew cask install vlc
+brew cask install google-chrome
+brew cask install firefox
+brew cask install mamp
+brew cask install sublime-text
+brew cask install sequel-pro
+brew cask install transmit
+brew cask install iterm2
+brew cask install spotify
+brew cask install skype
+brew cask install limechat
+brew cask install appcleaner
+brew cask install boom
+brew cask install dropbox
+brew cask install livereload
+brew cask install utorrent
+
+npm install -g grunt-cli
+gem install sass
